0a6a4327ebce548d1fc61b1c0ebc606a1eb9a793
smoon72 <sjm9219@gmail.com>
2023-12-11 21:20:40 -0500

    Moved to here

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Money Tracker';
+
+  addPurchase() {
+    console.log("purchased items");
+  }
+}
